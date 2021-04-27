772b87d82a845f5aa9f6191c3fd4d5f4055dec1c
Vickie Primes <xcrun git config --global user.email>
2021-04-26 21:02:12 -0400

    Final for Lab 14 - sandwiches

---
+++ Lab Sandwiches/Lab Sandwiches/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Lab Sandwiches
+//
+//  Created by Vickie Primes on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var sandwiches: [Sandwich] = []
+    
+    var body: some View {
+        NavigationView {
+           List(sandwiches) { sandwich in
+            NavigationLink(destination: Text(sandwich.name)) {
+                Image(sandwich.thumbnailName)
+                  .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount) ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+      
+          }
+            
+           }
+          .navigationTitle("Sandwiches")
+           }
+            
+        }
+        
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(sandwiches: testData)
+    }
+}
+
+
